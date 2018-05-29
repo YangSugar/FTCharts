a0b09b0fac225d0736566267cc802126349f3d5d
futang yang <yangfutang@jujin8.com>
2018-05-29 16:14:30 +0800

    添加基础

---
+++ FTCharts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FTCharts
+//
+//  Created by futang yang on 2018/5/29.
+//  Copyright © 2018年 futang yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
